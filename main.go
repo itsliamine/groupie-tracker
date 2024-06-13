fc9d0dbbc7c918dfae981a5c6f333996afacb776
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-13 17:53:57 +0200

    Search bar

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 const RED = "\033[31;1m"
@@ -34,11 +37,32 @@ type Date struct {
 	Dates []string `json:"dates"`
 }
 
+type SearchRequest struct {
+	Search string
+}
+
+type SearchReponse struct {
+	Artist Artist
+	Type   string
+}
+
+var artistsJson []Artist
+
 func main() {
 	log.SetFlags(log.Ltime)
 	log.SetPrefix("groupie-tracker:")
 
+	f, err := os.ReadFile("json/artists.json")
+	if err != nil {
+		log.Fatal("Error when reading json file", err)
+		return
+	}
+
+	json.Unmarshal(f, &artistsJson)
+
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/searchbar", searchBarHandler)
+	http.HandleFunc("/artist", artistHandler)
 
 	log.Println(GREEN, "Server started at http://localhost:8080", NONE)
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -52,8 +76,61 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func searchBarHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		badRequestHandler(w)
+	}
+
+	var s SearchRequest
+
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	var response []SearchReponse
+	for _, v := range artistsJson {
+		if strings.Contains(strings.ToLower(v.Name), strings.ToLower(s.Search)) {
+			r := SearchReponse{
+				Artist: v,
+				Type:   "artist/band",
+			}
+			response = append(response, r)
+		}
+		for _, member := range v.Members {
+			if strings.Contains(strings.ToLower(member), strings.ToLower(s.Search)) {
+				r := SearchReponse{
+					Artist: v,
+					Type:   "member",
+				}
+				response = append(response, r)
+			}
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func artistHandler(w http.ResponseWriter, r *http.Request) {
+	id := 1
+	var a Artist
+	for _, v := range artistsJson {
+		if v.Id == id {
+			a = v
+		}
+	}
+	t, _ := template.ParseFiles("templates/artist.html")
+	t.Execute(w, a)
+}
+
 func badRequestHandler(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusBadRequest)
 	t, _ := template.ParseFiles("templates/400.html")
 	t.Execute(w, nil)
 }
+
+// func internalServerErrorHandler(w http.ResponseWriter, err error) {
+// 	w.WriteHeader(http.StatusInternalServerError)
+// 	t, _ := template.ParseFiles("templates/500.html")
+// 	t.Execute(w, err)
+// }
