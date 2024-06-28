a90002124ed15db138a37e0f75f65a5fe24842ec
faoudia <fares.aoudia0@gmail.com>
2024-06-28 20:38:42 +0200

    Artists page, opitmized server handlers

--- templates/scripts/filters.js
+++ templates/scripts/filters.js
@@ -1,4 +1,5 @@
-window.onload = () => {
-	console.log("fil")
-	
-}
+document.getElementById("creation-year-value").innerHTML = document.getElementById("creation-year").value
+
+document.getElementById("creation-year").addEventListener("input", e => {
+	document.getElementById("creation-year-value").innerHTML = e.target.value
+})
